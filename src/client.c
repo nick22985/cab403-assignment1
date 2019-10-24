b2b2e5104c1f52775882ebfa646976160bdfbaaa
Kennnygo <dkenward@hotmail.com.au>
2019-10-24 19:53:45 +1000

    Version change/cleanup

--- src/client.c
+++ src/client.c
@@ -1,120 +1,45 @@
-// gcc -o client client.c
-//./server 1000
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <netinet/in.h>
+
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netdb.h>
 
-	#define PORT 54321    /* the port users will be connecting to */
-	#define MAXDATASIZE 100 /* max number of bytes we can get at once */
-	#define BUFFERSIZE  1000  //Size of memory buffer. I have no idea how big this should be.//
+#define DEFAULTPORT 12345
 
-	#define ARRAY_SIZE 1  //taken from wk7 array sending solution//
+int main(int argc, char *argv[]) {
+//create socket
+int network_socket;
+network_socket = socket(AF_INET, SOCK_STREAM,0);
 
-	void SetUpBuffer(){
-		char *ClientSideStorage = calloc(BUFFERSIZE, sizeof(char));
-	}
+//specify an address for the socket
+struct sockaddr_in server_address;
+server_address.sin_family = AF_INET;
+server_address.sin_port = htons(DEFAULTPORT);
+server_address.sin_addr.s_addr = INADDR_ANY;
 
-
-	void AnotherBufferSetup(){
-		char* ClientBuffer;
-		ClientBuffer = (char*)malloc(BUFFERSIZE);
-	}
-
-
-void Send_Array_Data(int socket_id, char *myArray) {
-	int i=0;
-	uint16_t statistics;  
-	for (i = 0; i < ARRAY_SIZE; i++) {
-		statistics = htons(myArray[i]);
-		send(socket_id, &statistics, sizeof(uint16_t), 0);
-	}
+int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+//connection error checking
+if (connection_status != 0){
+    printf("CONNECTION ERROR");
 }
 
-int main(int argc, char **argv)
-{
-int sockfd, numbytes;  
-	char buf[MAXDATASIZE];
-	struct hostent *he;
-	struct sockaddr_in their_addr; /* connector's address information */
-
-	if (argc != 2) {
-		fprintf(stderr,"usage: client hostname\n");
-		exit(1);
-	}
-
-	if ((he=gethostbyname(argv[1])) == NULL) {  /* get the host info */
-		herror("gethostbyname");
-		exit(1);
-	}
-
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
-
-	their_addr.sin_family = AF_INET;      /* host byte order */
-	their_addr.sin_port = htons(PORT);    /* short, network byte order */
-	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-	bzero(&(their_addr.sin_zero), 8);     /* zero the rest of the struct */
-
-	if (connect(sockfd, (struct sockaddr *)&their_addr, \
-	sizeof(struct sockaddr)) == -1) {
-		perror("connect");
-		exit(1);
-	}
-
-	if ((numbytes=recv(sockfd, buf, MAXDATASIZE, 0)) == -1) {
-		perror("recv");
-		exit(1);
-	}
-
-	buf[numbytes] = '\0';
-
-	printf("Received: %s",buf);
+//recieve data from server
+char server_response[256];
+recv(network_socket, &server_response, sizeof(server_response),0);
 
 
-	// while(1){   //While connected - breaking while loop ends client connection
-	// void TestMessageSending (){
-	// 	char message[60];
-	// 	scanf( "%s", message );
-	// 	send(int sockfd, )
-	// }
-	// }
+//print the server response
+printf("The server said %s\n", server_response);
 
-	/* Create an array of squares of first 20 whole numbers */
-	char *simpleArray[ARRAY_SIZE] ;
-	// for (int i = 0; i < ARRAY_SIZE; i++) {
-	// 	simpleArray[i] = "Test Message";
-	// }
+//close connection
+close(network_socket);
 
-	simpleArray[0] = "Test Message";
-	printf("%s \n",simpleArray[0]);
-	// printf("%c \n",simpleArray[1]);
-	// printf("%c \n",simpleArray[2]);
-	// printf("%c \n",simpleArray[3]);
-	// printf("%c \n",simpleArray[4]);
-	// printf("%c \n",simpleArray[5]);
-	// printf("%c \n",simpleArray[6]);
-	// printf("%c \n",simpleArray[7]);
-	// printf("%c \n",simpleArray[8]);
-	// printf("%c \n",simpleArray[9]);
-
-	Send_Array_Data(sockfd, simpleArray);
-
-
-
-
-
-	close(sockfd);
-
-
-	return 0;
+return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,139 +1,56 @@
-/****************************************************************
- * 																*
- * 								Libraries						*
- * 																*
- ****************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
-#include "../include/server.h"
-#include <arpa/inet.h>
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <signal.h>         /*SIGINT*/
-#include <stdbool.h>
-#include <errno.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <netinet/in.h> 
-#include <sys/socket.h> 
-#include <sys/wait.h> 
+#include <string.h>
+#include <strings.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <errno.h>
-#include <termios.h> 		// Stop Terminal Echo
-#include <pthread.h>
-#include <netinet/in.h> 
 #include <netdb.h>
 
-#define MYPORT 54321    /* the port users will be connecting to */
-#define BACKLOG 10     /* how many pending connections queue will hold */
+#define DEFAULTPORT 12345
 
-/****************************************************************
- * 																*
- * 						Initial Values					*
- * 																*
- ****************************************************************/
-
-	#define ARRAY_SIZE 1  /* Size of array to receive */
-	#define RETURNED_ERROR -1
-
-	int *Receive_Array_Int_Data(int socket_identifier, int size) {
-    int number_of_bytes, i=0;
-    uint16_t statistics;
-	
-	int *results = malloc(sizeof(int)*size);
-	for (i=0; i < size; i++) {
-		if ((number_of_bytes=recv(socket_identifier, &statistics, sizeof(uint16_t), 0))
-		         == RETURNED_ERROR) {
-			perror("recv");
-			exit(EXIT_FAILURE);			
-		    
-		}
-		results[i] = ntohs(statistics);
-	}
-	return results;
-}
-
-// void send_response(int socket_fd, req_t response){
-//     if (send(socket_fd, &response, sizeof(req_t), PF_UNSPEC) == ERROR){
-//         perror("Sending request response");
-//     }
-// }
-
-///////////////////////////////////////////////////////////////////
-
-int main(int argc, char *argv[]) {
-    int sockfd, new_fd;  /* listen on sock_fd, new connection on new_fd */
-	struct sockaddr_in my_addr;    /* my address information */
-	struct sockaddr_in their_addr; /* connector's address information */
-	socklen_t sin_size;
-    int client_number = 0;
-
-	/* generate the socket */
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
-
-	/* generate the end point */
-	my_addr.sin_family = AF_INET;         /* host byte order */
-	my_addr.sin_port = htons(MYPORT);     /* short, network byte order */
-	my_addr.sin_addr.s_addr = INADDR_ANY; /* auto-fill with my IP */
-		/* bzero(&(my_addr.sin_zero), 8);   ZJL*/     /* zero the rest of the struct */
-
-	/* bind the socket to the end point */
-	if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) \
-	== -1) {
-		perror("bind");
-		exit(1);
-	}
-
-	/* start listnening */
-	if (listen(sockfd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
-
-	printf("server starts listnening ...\n");
-
-	/* repeat: accept, send, close the connection */
-	/* for every accepted connection, use a sepetate process or thread to serve it */
-	while(1) {  /* main accept() loop */
-		sin_size = sizeof(struct sockaddr_in);
-		if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, \
-		&sin_size)) == -1) {
-			perror("accept");
-			continue;
-		}
-		printf("server: got connection from %s\n", \
-			inet_ntoa(their_addr.sin_addr));
-        client_number += 1;
-        printf("Client number %d connected\n", client_number);
-        // char *temp = "test";
-		if (!fork()) { /* this is the child process */
-			if (send(new_fd, "You're connected!\n", 100, 0) == -1)
-				perror("send");
-
-						/* Call method to recieve array data */
-			int *results = Receive_Array_Int_Data(new_fd,  ARRAY_SIZE);	
-
-			/* Print out the array results sent by client */
-			for (int i=0; i < ARRAY_SIZE; i++) {
-				printf("Value of index[%d] = %c\n", i, results[i]);
-			}			
-
-			free(results);
-
-			if (send(new_fd, "All of array data received by server\n", 40 , 0) == -1)
-				perror("send");
-			close(new_fd);
-			exit(0);
-			close(new_fd);
-			exit(0);
-		}
-		close(new_fd);  /* parent doesn't need this */
-
-		while(waitpid(-1,NULL,WNOHANG) > 0); /* clean up child processes */
-    }
+void error(const char *msg){
+    perror(msg);
+    exit(1);
 }
 
 
+int main(int argc, char *argv[]){
 
+    int PortUsed = DEFAULTPORT;
+
+    char server_message[256] = "You have reached the server. Yes cunt.";
+
+//create server socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM,0);
+
+    //address structure
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PortUsed);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    //bind the socket to specified IP and port
+    bind (server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+    //listen for connections
+    listen(server_socket, 10);
+
+    
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL);
+
+
+    //send message
+    send(client_socket, server_message, sizeof(server_message),0);
+
+    //close socket
+    close(server_socket);
+
+    return 0;
+}
