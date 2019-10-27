b9745ee56f2adf48f3388b3cf4fd67cfc5548afe
nick22985 <n10224378@qut.edu.au>
2019-10-27 17:01:02 +1000

    merge

--- src/server.c
+++ src/server.c
@@ -17,10 +17,13 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <signal.h>
+#include <stdlib.h>
 
 #define SHARED_OBJECT_PATH         "/messenger"
 #define DEFAULTPORT 12345
 #define server_buff 256
+
 
 void error(const char *msg){
     perror(msg);
@@ -32,6 +35,12 @@ void SendMessage(int DestinationSocket ,char *EnteredText){
 	send(DestinationSocket, EnteredText, strlen(EnteredText), 0);
 	//printf("sent !\n");
 	//printf("TestServer: %ld\n",strlen(EnteredText));
+}
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+    keepRunning = 0;
 }
 
 int SelectPort(int EnteredPort) {
@@ -71,40 +80,14 @@ int main(int argc, char *argv[]){
         int time;
         char message;
         }thevaultpacket_t;
-
+    
     char server_message[256] = "You have reached the server. Yes cunt.\n";
 
+    signal(SIGINT, intHandler);
     //start clock
     time_t Server_Starttime = time(NULL);
     struct timeval start, end;
     gettimeofday(&start, NULL);
-
-    //MEMORY
-
-    // if (shm_unlink(SHARED_OBJECT_PATH) != 0) {
-	// 	perror("In shm_unlink()");
-	// 	exit(1);
-	// }
-	// int shared_seg_size = (sizeof(thevaultpacket_t));   /* We want a shared segment capable of storing one message */
-	// thevaultpacket_t* shared_msg;      /* the shared segment, and head of the messages list */
-
-	// fd = shm_open(SHARED_OBJECT_PATH, O_CREAT | O_EXCL | O_RDWR, S_IRWXU | S_IRWXG);
-    
-	// if (fd < 0) {
-	// 	perror("In shm_open()");
-	// 	exit(1);
-	// }
-	// fprintf(stderr, "Created shared memory object %p\n", SHARED_OBJECT_PATH);
-
-    // ftruncate(fd, shared_seg_size);
-    // //messageBank *messageBankPtr = mmap(NULL, sizeof(messageBank), PROT_WRITE | PROT_READ, MAP_SHARED| MAP_ANONYMOUS, -1, 0);
-    // shared_msg = (thevaultpacket_t*)mmap(NULL, shared_seg_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	// if (shared_msg == NULL) {
-	// 	perror("In mmap()");
-	// 	exit(1);
-	// }
-    // char * SampleArray[10];
-
 	//Int to refer to Buffer by
 	int n;
     int ChosenPort = DEFAULTPORT;
@@ -128,7 +111,6 @@ int main(int argc, char *argv[]){
 
     //bind the socket to specified IP and port
     bind (server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
 	while(1){
 		//listen for connections
 		listen(server_socket, 10);
@@ -138,18 +120,10 @@ int main(int argc, char *argv[]){
 
         //send message
         send(client_socket, server_message, sizeof(server_message),0);
-        while(1){
             bzero(buffer,256);  
             n = read(client_socket,buffer,256);
             if (strlen(buffer) != 0) {
                 printf("Client: %s\n",buffer);
-                //SampleArray[0] = buffer;
-                //printf("SampleArray position 0 is --> %s\n", SampleArray[0]);
-                //printf("%d\n", strcmp("next", buffer));
-                // if ( strcmp("next", buffer) == 0) {
-                //     printf("Test Function Here\n");
-                //     //Function for server to run when next is run.
-                // }
                 if ( strcmp("next CHANNELID", buffer) == 0) {
                     printf("NEXT CHANNELID Here\n");
                     //Function for server to run when next is run.
@@ -184,50 +158,20 @@ int main(int argc, char *argv[]){
                 //     //Function for server to run when next is run.
 
                 // }
-                    else {
-                        printf("NOT RECOGNISED COMMAND\n");
-                        //printf("length of buffer is: %ld\n", strlen(buffer)); 
-                        
-                        //Prints time of message sending
-                        gettimeofday(&end, NULL);
-                        long seconds = (end.tv_sec - start.tv_sec);
-                        long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
-                        printf("Time elpased is %ld seconds and %ld micros\n", seconds, micros);
-                        
-                        //Formating lines to send to client
-                        //clear the buffer for use again
-                        // char temp1format;
-                        // char temp2format;
-                        // temp1format = strcat("|", buffer); 
-                        // temp2format = strcat(temp1format, "|"); 
-                        // temp1format = strcat("|", temp2format); 
-                        // temp2format = strcat(temp1format, "|"); 
-                        // printf("%d", temp1format);
-                        // char temp1format;
-                        // char temp2format;
-                        // temp1format = strcat("|", buffer); 
-                        // temp2format = strcat(temp1format, "|"); 
-                        // temp1format = strcat("|", temp2format); 
-                        // // temp2format = strcat(temp1format, "|"); 
-                        // printf("%d", temp1format);
-                        // char temp = "|";
-                        // sprintf(temp,buffer,"|");
-                        // printf("%s", temp);
-                        SendMessage(client_socket, buffer);
-                        //clear the buffer for use again
-                        bzero(buffer,sizeof(buffer));
-
+                else {
+                    printf("NOT RECOGNISED COMMAND\n");
+                    gettimeofday(&end, NULL);
+                    long seconds = (end.tv_sec - start.tv_sec);
+                    long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+                    printf("Time elpased is %ld seconds and %ld micros\n", seconds, micros);
+                    SendMessage(client_socket, buffer);
+                    //clear the buffer for use again
+                    bzero(buffer,sizeof(buffer));
                     }
-            }
-        }
-	}
+                }
+	    }
 
-    if (shm_unlink(SHARED_OBJECT_PATH) != 0) {
-		perror("In shm_unlink()");
-		exit(1);
-	}
-
-	//close socket
+	//close socketx
     close(server_socket);
 
     return 0;
