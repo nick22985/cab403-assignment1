77f6f611beb9e4351e0597dae8002782825b3355
nick22985 <n10224378@qut.edu.au>
2019-10-27 17:58:40 +1000

    old server still getting core on my end

--- src/server.c
+++ src/server.c
@@ -17,13 +17,10 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <signal.h>
-#include <stdlib.h>
 
 #define SHARED_OBJECT_PATH         "/messenger"
 #define DEFAULTPORT 12345
 #define server_buff 256
-
 
 void error(const char *msg){
     perror(msg);
@@ -35,12 +32,6 @@ void SendMessage(int DestinationSocket ,char *EnteredText){
 	send(DestinationSocket, EnteredText, strlen(EnteredText), 0);
 	//printf("sent !\n");
 	//printf("TestServer: %ld\n",strlen(EnteredText));
-}
-
-static volatile int keepRunning = 1;
-
-void intHandler(int dummy) {
-    keepRunning = 0;
 }
 
 int SelectPort(int EnteredPort) {
@@ -63,14 +54,16 @@ int main(int argc, char *argv[]){
         int time;
         char message;
         }thevaultpacket_t;
-    
+
     char server_message[256] = "You have reached the server. Yes cunt.\n";
 
-    signal(SIGINT, intHandler);
     //start clock
     time_t Server_Starttime = time(NULL);
     struct timeval start, end;
     gettimeofday(&start, NULL);
+
+ 
+
 	//Int to refer to Buffer by
 	int n;
     int ChosenPort = DEFAULTPORT;
@@ -94,7 +87,8 @@ int main(int argc, char *argv[]){
 
     //bind the socket to specified IP and port
     bind (server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-	while(keepRunning){
+
+	while(1){
 		//listen for connections
 		listen(server_socket, 10);
     	int client_socket;
@@ -103,58 +97,29 @@ int main(int argc, char *argv[]){
 
         //send message
         send(client_socket, server_message, sizeof(server_message),0);
+        while(1){
             bzero(buffer,256);  
             n = read(client_socket,buffer,256);
             if (strlen(buffer) != 0) {
                 printf("Client: %s\n",buffer);
-                if ( strcmp("next CHANNELID", buffer) == 0) {
-                    printf("NEXT CHANNELID Here\n");
-                    //Function for server to run when next is run.
-                }
-                else if ( strcmp("sub CHANNELID", buffer) == 0) {
-                    printf("SUB function Here\n");
-                    //Function for server to run when next is run.
-                }
-                else if ( strcmp("unsub CHANNELID", buffer) == 0) {
-                    printf("UNSUB function Here\n");
-                    //Function for server to run when next is run.
-                }
-                else if ( strcmp("livefeed CHANNELID", buffer) == 0) {
-                    printf("LIVEFEED CHANNELID Here\n");
-                    //Function for server to run when next is run.
-                }
-                else if ( strcmp("livefeed", buffer) == 0) {
-                    printf("LIVEFEED Here\n");
-                    //Function for server to run when next is run.
-                }
-                else if ( strcmp("SEND CHANNELID MESSAGE", buffer) == 0) {
-                    printf("SEND Here\n");
-                    //Function for server to run when next is run.
-                }
-                // else if ( strcmp("CHANNELS", buffer) == 0){
-                //     printf("CHANNELS .........");
-                //     //Function for server to when next is run
 
-                // }
-                //     else if ( strncmp("BYE", buffer, 3) == 0) {
-                //     printf("BYE Here\n");
-                //     //Function for server to run when next is run.
+                       //Prints time of message sending
+                        gettimeofday(&end, NULL);
+                        long seconds = (end.tv_sec - start.tv_sec);
+                        long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
+                        printf("Time elpased is %ld seconds and %ld micros\n", seconds, micros);
 
-                // }
-                else {
-                    printf("NOT RECOGNISED COMMAND\n");
-                    gettimeofday(&end, NULL);
-                    long seconds = (end.tv_sec - start.tv_sec);
-                    long micros = ((seconds * 1000000) + end.tv_usec) - (start.tv_usec);
-                    printf("Time elpased is %ld seconds and %ld micros\n", seconds, micros);
-                    SendMessage(client_socket, buffer);
-                    //clear the buffer for use again
-                    bzero(buffer,sizeof(buffer));
+                        SendMessage(client_socket, buffer);
+                        //clear the buffer for use again
+                        bzero(buffer,sizeof(buffer));
+
                     }
-                }
-	    }
+            }
+        }
+	
 
-	//close socketx
+
+	//close socket
     close(server_socket);
 
     return 0;
